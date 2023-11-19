7c2d5de0edda3f820d6f0a6ab9953288e521f5f5
trickrocha <github.comgithub.com/trickrocha>
2023-11-19 20:30:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var elementoDuvida = document.querySelectorAll('.duvida')
+
+elementoDuvida.forEach(function(duvida){
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
+    })
+})
+
